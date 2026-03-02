c01aac98124f55e6ac1d979b0e3fd43ce5277f71
Vatan Waikar <vatanwaikar@gmail.com>
2026-03-02 11:51:54 +0530

    changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-const baseUrl = "https://www.prosira.in";
+const baseUrl = "https://prosira.in";
   return [
     {
       url: baseUrl,
